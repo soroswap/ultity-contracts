a8ef60d1886ea9197477a444e03640e8c9c52c9c
coderipper <hello@joaquinsoza.dev>
2024-05-17 13:18:52 +0200

    rust tests working

--- liquidity_timelock/src/test/claim.rs
+++ liquidity_timelock/src/test/claim.rs
@@ -1,7 +1,8 @@
 extern crate chrono;
 
 use crate::test::AddLiqudityTimelockTest;
-use chrono::Utc;
+use chrono::{Duration, Utc};
+use soroban_sdk::testutils::Ledger;
 
 #[test]
 fn claim_test() {
@@ -42,3 +43,43 @@ fn claim_test() {
 
   assert_eq!(result,());
 }
+
+// #[test]
+// fn claim_test_not_endtime() {
+//   let test = AddLiqudityTimelockTest::setup();
+
+//   // ADDING LIQUIDITY FIRST
+//   let now = Utc::now();
+//   // END TIMESTAMP SET TO CURRENT TIMESTAMP, IT SHOULD BE ABLE TO CLAIM
+//   let end_timestamp = (now + Duration::hours(1)).timestamp() as u64;
+
+//   test.timelock_contract.initialize(
+//     &test.admin, 
+//     &test.router_contract.address,
+//     &end_timestamp
+//   );
+
+//   let amount_0: i128 = 1_000_000_000_000;
+//   let amount_1: i128 = 4_000_000_000_000;
+//   let expected_liquidity: i128 = 2_000_000_000_000;
+
+//   let (added_token_0, added_token_1, added_liquidity) = test.timelock_contract.add_liquidity(
+//     &test.token_0,
+//     &test.token_1,
+//     &amount_0,
+//     &amount_1,
+//     &test.admin,
+//     &end_timestamp
+//   );
+
+//   assert_eq!(added_token_0, amount_0);
+//   assert_eq!(added_token_1, amount_1);
+//   assert_eq!(added_liquidity, expected_liquidity);
+//   // END OF ADDING LIQUIDITY
+
+//   let result = test.timelock_contract.claim(
+//     &test.pair_address
+//   ); 
+
+//   assert_eq!(result,());
+// }
