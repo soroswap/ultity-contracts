5d8c1a81559fa89850664d56326dfecb498601cd
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-07-25 14:16:30 +0200

    test get_release_time

--- liquidity_timelock/src/test/initialize.rs
+++ liquidity_timelock/src/test/initialize.rs
@@ -2,23 +2,38 @@ use crate::error::CombinedLiquidityTimelockError;
 use crate::test::AddLiqudityTimelockTest;
 
 #[test]
-fn test_initialize_and_get_admin() {
+fn test_initialize_and_get_values() {
     let test = AddLiqudityTimelockTest::setup();
+    let desired_release_time = 1746885472;
 
     test.timelock_contract.initialize(
         &test.admin, 
         &test.router_contract.address,
-        &1746885472
+        &desired_release_time
     );
 
     let admin = test.timelock_contract.get_admin();
     assert_eq!(admin, test.admin);
+
+    // test release time
+    let release_time = test.timelock_contract.get_release_time();
+    assert_eq!(release_time, desired_release_time);
+
 }
 
 #[test]
 fn test_get_admin_not_yet_initialized() {
     let test = AddLiqudityTimelockTest::setup();
     let result = test.timelock_contract.try_get_admin();
+
+    assert_eq!(result, Err(Ok(CombinedLiquidityTimelockError::TimelockNotInitialized)));
+}
+
+
+#[test]
+fn test_get_release_time_not_yet_initialized() {
+    let test = AddLiqudityTimelockTest::setup();
+    let result = test.timelock_contract.try_get_release_time();
 
     assert_eq!(result, Err(Ok(CombinedLiquidityTimelockError::TimelockNotInitialized)));
 }
